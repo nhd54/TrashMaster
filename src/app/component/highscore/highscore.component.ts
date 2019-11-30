2ffdb3ca8a62523d69f2eed47ee8dadce62f6aa1
Rasmus <rasm995j@edu.easj.dk>
2019-11-30 18:05:17 +0100

    changes to styles.css and added icon to assets. Additionally added content to component/level & component highscore

--- src/app/component/highscore/highscore.component.ts
+++ src/app/component/highscore/highscore.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./highscore.component.css']
 })
 export class HighscoreComponent implements OnInit {
+  one = 'player1';
+  two = 'player2';
+  three = 'player3';
+  four = 'player4';
+  five = 'player5';
+  six = 'player6';
+  seven = 'player7';
+  eight = 'player8';
+  nine = 'player9';
+  ten = 'player10';
+
 
   constructor() { }
 
