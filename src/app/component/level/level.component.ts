6ce31bf1002145a016cf1e24ce604b93a3476d95
BuildTools <unconfigured@null.spigotmc.org>
2019-12-11 12:02:51 +0100

    added game difficulty

--- src/app/component/level/level.component.ts
+++ src/app/component/level/level.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LevelComponent implements OnInit {
 
+  gameDifficulty = "easy";
+
+  changeDifficulty(x) {
+    var x;
+
+    if(x == 0) {
+      this.gameDifficulty = "easy";
+    }
+    if(x == 1) {
+      this.gameDifficulty = "medium";
+    } 
+    if(x == 2) {
+      this.gameDifficulty = "hard";
+    }
+  }
+
+  debug() {
+    console.log(this.gameDifficulty);
+  }
+
+  changeColor(x) {
+    var x;
+    if(x == 0) {
+      document.querySelector("#iconEasy").src = "../assets/img/trash3.svg";
+      document.querySelector("#iconMed").src = "../assets/img/trashgrey.svg";
+      document.querySelector("#iconHard").src = "../assets/img/trashgrey.svg";
+    }
+    if(x == 1) {
+      document.querySelector("#iconEasy").src = "../assets/img/trashgrey.svg";
+      document.querySelector("#iconMed").src = "../assets/img/trash3.svg";
+      document.querySelector("#iconHard").src = "../assets/img/trashgrey.svg";
+    }
+    if(x == 2) {
+      document.querySelector("#iconEasy").src = "../assets/img/trashgrey.svg";
+      document.querySelector("#iconMed").src = "../assets/img/trashgrey.svg";
+      document.querySelector("#iconHard").src = "../assets/img/trash3.svg";
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
--- src/app/component/play/play.component.ts
+++ src/app/component/play/play.component.ts
@@ -10,6 +10,23 @@ export class PlayComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let difficulty;
+    let url = window.location.href;
+    
+    difficulty = url.replace(/\/+$/, '').split("=").pop();
+
+    if(difficulty == "easy") {
+      document.querySelector(".diffMed").classList.add("displayNone");
+      document.querySelector(".diffHard").classList.add("displayNone");
+      console.log("Game is set to " + difficulty);
+    }
+    if(difficulty == "medium") {
+      document.querySelector(".diffHard").classList.add("displayNone");
+      console.log("Game is set to " + difficulty);
+    }
+    if(difficulty == "hard") {
+      console.log("Game is set to " + difficulty);
+    }
   }
 
 }
